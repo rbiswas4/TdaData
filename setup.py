b3ab2c22491d109801dddb138b31249c21db203f
R. Biswas <rbiswas4@gmail.com>
2020-04-17 04:50:23 +0200

    Added a very basic test on reading simsurvey. Basically checks that reading is done, but there could be much more detailed checks.
    - Added test
    	new file:   tests/test_simsurvey.py
    - Fixing bug: import tdd first
    	modified:   tests/test_simsurvey.py
    - Changing install_package
    	modified:   install/install_package.sh
    - Fixing bug in setup.py: repeated `install_requires`
    	modified:   setup.py

--- setup.py
+++ setup.py
@@ -34,8 +34,7 @@ setup(# package information
       packagedir={PACKAGENAME: 'tdd'},
       # What data to include as packages
       include_package_data=True,
-      install_requires=['astropy'],
+      install_requires=['astropy', 'simsurvey'],
       package_data={PACKAGENAME:['example_data/*.csv',
                                  'example_data/*.pkl']},
-      install_requires=['simsurvey']
       )
---
+++ tests/test_simsurvey.py
@@ -0,0 +1,23 @@
+import os
+import pandas as pd
+import tdd
+from tdd import read_simsurvey, example_data
+
+def test_read_simsurvey():
+    lcs_fname  = os.path.join(example_data,
+                              'salt2_ex_lcs.pkl')
+    params_fname = os.path.join(example_data,
+                                'salt2_ex_params.csv')
+
+    # read in params, and replace index name
+    params = pd.read_csv(params_fname)
+    params.rename(columns=dict(idx='tid'), inplace=True)
+    params.set_index('tid', inplace=True)
+
+    meta_all, phot_all = read_simsurvey(lcs_fname, shift_idx=0, sim_suffix=0,
+                                        meta_selection_func=None,
+                                        params=params, phot_selection=None)
+    
+    assert len(meta_all) == 8
+
+    return meta_all, phot_all
